f9b103289bc4af1f210f0856cd3fb15f33e3b590
Mobying <1643317524@qq.com>
2017-05-06 23:20:52 +0800

    Initial commit with OpenCV

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,12 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+extern "C"
+jstring
+Java_bying_imageprotect_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
